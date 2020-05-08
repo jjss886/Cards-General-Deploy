e8c8d4336d2587ed914b64fd98f240deb793f6cd
James Shen <jjss886@gmail.com>
2020-05-08 11:03:25 -0400

    laying out next player function in cp comp

--- client/components/Game/ControlPanel.js
+++ client/components/Game/ControlPanel.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { drawCard } from "../../store";
+import { drawCard, nextPlayer } from "../../store";
 
 class ControlPanel extends Component {
   drawing = () => {
@@ -8,27 +8,48 @@ class ControlPanel extends Component {
     drawCard(user, deck.slice(-1));
   };
 
+  next = () => {
+    const { user, players, nextPlayer } = this.props,
+      lenIdx = players.length - 1,
+      curIdx = players.indexOf(user);
+
+    if (lenIdx === curIdx) nextPlayer(players[0]);
+    else nextPlayer(players[curIdx + 1]);
+  };
+
   render() {
+    const { user, livePlayer } = this.props;
+
     return (
       <div className="cpFullDiv">
         <h3>Control Panel</h3>
 
-        <button type="button" className="gBtn">
-          Draw
-        </button>
+        {user && user === livePlayer ? (
+          <>
+            <button type="button" className="gBtn">
+              Draw
+            </button>
 
-        <button type="button" className="gBtn">
-          Next
-        </button>
+            <button type="button" className="gBtn">
+              Next
+            </button>
+          </>
+        ) : null}
       </div>
     );
   }
 }
 
-const mapState = (state) => ({ user: state.user, deck: state.deck });
+const mapState = (state) => ({
+  user: state.user,
+  deck: state.deck,
+  players: state.channel.players,
+  livePlayer: state.channel.livePlayer,
+});
 
 const mapDispatch = (dispatch) => ({
   drawCard: (name, card) => dispatch(drawCard(name, card)),
+  nextPlayer: (name) => dispatch(nextPlayer(name)),
 });
 
 export default connect(mapState, mapDispatch)(ControlPanel);
